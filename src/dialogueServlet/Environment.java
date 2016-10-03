79691ae47039994f62004f845ae2c8a8969e0def
Amna Irfan <amna.irfan@icloud.com>
2016-10-03 13:37:50 -0500

    added environment java class to git ignore

--- src/dialogueServlet/Environment.java
+++
@@ -1,6 +0,0 @@
-package dialogueServlet;
-
-public class Environment {
-	public final static String TAICHI_DB = "jdbc:sqlite:/Users/amnairfan/Documents/eclipse_workspace/DialogueSystems/dialogueSystem.db";
-	public final static String TAICHI_AGENT = "/Users/amnairfan/DialogueSystem/code/DialogueAgent/data/";
-}
